376846b715b05aa6e697f3d7ca7c27e47b170a0e
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2023-11-26 07:29:58 +0800

    Add files via upload

---
+++ tools/tools-3/rata-rata-nilai.js
@@ -0,0 +1,404 @@
+// NILAI RAPOR SEMESTER 1 - 5
+function subtituteData() {
+  const pai = [84, 85, 92, 89, 94, 97, ""
+  ];
+  const ppkn = [82, 82, 87, 87, 92, 93, ""
+  ];
+  const bind = [85, 85, 85, 85, 95, 98, ""
+  ];
+  const mtkw = [90, 90, 92, 92, 96, 98, ""
+  ];
+  const sjr = [90, 91, 90, 91, 85, 97, ""
+  ];
+  const bing = [90, 90, 81, 81, 96, 96, ""
+  ];
+  const snb = [83, 82, 85, 81, 89, 92, ""
+  ];
+  const pjok = [80, 90, 85, 94, 87, 90, ""
+  ];
+  const pkwu = [84, 87, 86, 89, 94, 97, "" 
+  ];
+  const mtkp = [85, 85, 90, 90, 96, 99, ""
+  ];
+  const bio = [94, 95, 90, 90, 88, 94, ""
+  ];
+  const fis = [76, 75, 73, 74, 95, 98, ""
+  ];
+  const kim = [86, 86, 89, 89, 91, 95, ""
+  ];
+  const eko = [85, 86, 85, 86, 86, 92, ""
+  ];
+  const geo = [89, 89, 90, 91
+  ];
+  const mlo = [88, 88, 95, 95
+  ];
+ 
+
+// DATA INPUT NILAI KE HTML
+  document.getElementById("pai-s1-p").innerText = pai[0];
+  document.getElementById("pai-s1-k").innerText = pai[1];
+  document.getElementById("pai-s2-p").innerText = pai[2];
+  document.getElementById("pai-s2-k").innerText = pai[3];
+  document.getElementById("pai-s3").innerText = pai[4];
+  document.getElementById("pai-s4").innerText = pai[5];
+  document.getElementById("pai-s5").innerText = pai[6];
+  
+  document.getElementById("ppkn-s1-p").innerText = ppkn[0];
+  document.getElementById("ppkn-s1-k").innerText = ppkn[1];
+  document.getElementById("ppkn-s2-p").innerText = ppkn[2];
+  document.getElementById("ppkn-s2-k").innerText = ppkn[3];
+  document.getElementById("ppkn-s3").innerText = ppkn[4];
+  document.getElementById("ppkn-s4").innerText = ppkn[5];
+  document.getElementById("ppkn-s5").innerText = ppkn[6];
+
+  document.getElementById("bind-s1-p").innerText = bind[0];
+  document.getElementById("bind-s1-k").innerText = bind[1];
+  document.getElementById("bind-s2-p").innerText = bind[2];
+  document.getElementById("bind-s2-k").innerText = bind[3];
+  document.getElementById("bind-s3").innerText = bind[4];
+  document.getElementById("bind-s4").innerText = bind[5];
+  document.getElementById("bind-s5").innerText = bind[6];
+
+  document.getElementById("mtkw-s1-p").innerText = mtkw[0];
+  document.getElementById("mtkw-s1-k").innerText = mtkw[1];
+  document.getElementById("mtkw-s2-p").innerText = mtkw[2];
+  document.getElementById("mtkw-s2-k").innerText = mtkw[3];
+  document.getElementById("mtkw-s3").innerText = mtkw[4];
+  document.getElementById("mtkw-s4").innerText = mtkw[5];
+  document.getElementById("mtkw-s5").innerText = mtkw[6];
+
+  document.getElementById("sjr-s1-p").innerText = sjr[0];
+  document.getElementById("sjr-s1-k").innerText = sjr[1];
+  document.getElementById("sjr-s2-p").innerText = sjr[2];
+  document.getElementById("sjr-s2-k").innerText = sjr[3];
+  document.getElementById("sjr-s3").innerText = sjr[4];
+  document.getElementById("sjr-s4").innerText = sjr[5];
+  document.getElementById("sjr-s5").innerText = sjr[6];
+
+  document.getElementById("bing-s1-p").innerText = bing[0];
+  document.getElementById("bing-s1-k").innerText = bing[1];
+  document.getElementById("bing-s2-p").innerText = bing[2];
+  document.getElementById("bing-s2-k").innerText = bing[3];
+  document.getElementById("bing-s3").innerText = bing[4];
+  document.getElementById("bing-s4").innerText = bing[5];
+  document.getElementById("bing-s5").innerText = bing[6];
+
+  document.getElementById("snb-s1-p").innerText = snb[0];
+  document.getElementById("snb-s1-k").innerText = snb[1];
+  document.getElementById("snb-s2-p").innerText = snb[2];
+  document.getElementById("snb-s2-k").innerText = snb[3];
+  document.getElementById("snb-s3").innerText = snb[4];
+  document.getElementById("snb-s4").innerText = snb[5];
+  document.getElementById("snb-s5").innerText = snb[6];
+
+  document.getElementById("pjok-s1-p").innerText = pjok[0];
+  document.getElementById("pjok-s1-k").innerText = pjok[1];
+  document.getElementById("pjok-s2-p").innerText = pjok[2];
+  document.getElementById("pjok-s2-k").innerText = pjok[3];
+  document.getElementById("pjok-s3").innerText = pjok[4];
+  document.getElementById("pjok-s4").innerText = pjok[5];
+  document.getElementById("pjok-s5").innerText = pjok[6];
+
+  document.getElementById("pkwu-s1-p").innerText = pkwu[0];
+  document.getElementById("pkwu-s1-k").innerText = pkwu[1];
+  document.getElementById("pkwu-s2-p").innerText = pkwu[2];
+  document.getElementById("pkwu-s2-k").innerText = pkwu[3];
+  document.getElementById("pkwu-s3").innerText = pkwu[4];
+  document.getElementById("pkwu-s4").innerText = pkwu[5];
+  document.getElementById("pkwu-s5").innerText = pkwu[6];
+
+  document.getElementById("mtkp-s1-p").innerText = mtkp[0];
+  document.getElementById("mtkp-s1-k").innerText = mtkp[1];
+  document.getElementById("mtkp-s2-p").innerText = mtkp[2];
+  document.getElementById("mtkp-s2-k").innerText = mtkp[3];
+  document.getElementById("mtkp-s3").innerText = mtkp[4];
+  document.getElementById("mtkp-s4").innerText = mtkp[5];
+  document.getElementById("mtkp-s5").innerText = mtkp[6];
+
+  document.getElementById("bio-s1-p").innerText = bio[0];
+  document.getElementById("bio-s1-k").innerText = bio[1];
+  document.getElementById("bio-s2-p").innerText = bio[2];
+  document.getElementById("bio-s2-k").innerText = bio[3];
+  document.getElementById("bio-s3").innerText = bio[4];
+  document.getElementById("bio-s4").innerText = bio[5];
+  document.getElementById("bio-s5").innerText = bio[6];
+
+document.getElementById("fis-s1-p").innerText = fis[0];
+document.getElementById("fis-s1-k").innerText = fis[1];
+  document.getElementById("fis-s2-p").innerText = fis[2];
+  document.getElementById("fis-s2-k").innerText = fis[3];
+  document.getElementById("fis-s3").innerText = fis[4];
+  document.getElementById("fis-s4").innerText = fis[5];
+  document.getElementById("fis-s5").innerText = fis[6];
+
+  document.getElementById("kim-s1-p").innerText = kim[0];
+  document.getElementById("kim-s1-k").innerText = kim[1];
+  document.getElementById("kim-s2-p").innerText = kim[2];
+  document.getElementById("kim-s2-k").innerText = kim[3];
+  document.getElementById("kim-s3").innerText = kim[4];
+  document.getElementById("kim-s4").innerText = kim[5];
+  document.getElementById("kim-s5").innerText = kim[6];
+
+  document.getElementById("eko-s1-p").innerText = eko[0];
+  document.getElementById("eko-s1-k").innerText = eko[1];
+  document.getElementById("eko-s2-p").innerText = eko[2];
+  document.getElementById("eko-s2-k").innerText = eko[3];
+  document.getElementById("eko-s3").innerText = eko[4];
+  document.getElementById("eko-s4").innerText = eko[5];
+  document.getElementById("eko-s5").innerText = eko[6];
+
+  document.getElementById("geo-s1-p").innerText = geo[0];
+  document.getElementById("geo-s1-k").innerText = geo[1];
+  document.getElementById("geo-s2-p").innerText = geo[2];
+  document.getElementById("geo-s2-k").innerText = geo[3];
+
+  document.getElementById("mlo-s1-p").innerText = mlo[0];
+  document.getElementById("mlo-s1-k").innerText = mlo[1];
+  document.getElementById("mlo-s2-p").innerText = mlo[2];
+  document.getElementById("mlo-s2-k").innerText = mlo[3];
+  
+  
+  
+// TOTAL DATA TIAP SEMESTER
+const arrays = [pai, ppkn, bind, mtkw, sjr, bing, snb, pjok, pkwu, mtkp, bio, fis, kim, eko, geo, mlo];
+
+let jumlahSemester1p = 0;
+let jumlahSemester1k = 0;
+for (const arr of arrays) {
+  if (arr.length > 0) {
+    jumlahSemester1p += arr[0];
+    if (arr.length > 1) {
+      jumlahSemester1k += arr[1];
+    }
+  }
+}
+const jumlahSemester1 = jumlahSemester1p + jumlahSemester1k;
+document.getElementById("jumlahSemester1").innerText = jumlahSemester1;
+
+let jumlahSemester2p = 0;
+let jumlahSemester2k = 0;
+for (const arr of arrays) {
+  if (arr.length > 2) {
+    jumlahSemester2p += arr[2];
+    if (arr.length > 3) {
+      jumlahSemester2k += arr[3];
+    }
+  }
+}
+const jumlahSemester2 = jumlahSemester2p + jumlahSemester2k;
+document.getElementById("jumlahSemester2").innerText = jumlahSemester2;
+
+let jumlahSemester3 = 0;
+for (const arr of arrays) {
+  if (arr.length > 4) {
+    jumlahSemester3 += arr[4];
+  }
+}
+document.getElementById("jumlahSemester3").innerText = jumlahSemester3;
+
+let jumlahSemester4 = 0;
+for (const arr of arrays) {
+  if (arr.length > 5) {
+    jumlahSemester4 += arr[5];
+  }
+}
+document.getElementById("jumlahSemester4").innerText = jumlahSemester4;
+
+let jumlahSemester5 = "";
+for (const arr of arrays) {
+  if (arr.length > 6) {
+    jumlahSemester5 += arr[6];
+  }
+}
+document.getElementById("jumlahSemester5").innerText = jumlahSemester5;
+
+
+
+// TOTAL DATA SEMESTER 1 - 5
+const jumlahTotalSemester = jumlahSemester1 + jumlahSemester2 + jumlahSemester3 + jumlahSemester4 + jumlahSemester5;
+document.getElementById("jumlahTotalSemester").innerText = jumlahTotalSemester;
+
+
+
+// RATA-RATA TIAP SEMESTER
+const rataRataSemester1 = jumlahSemester1 / 32;
+const rataRataSemester2 = jumlahSemester2 / 32;
+const rataRataSemester3 = jumlahSemester3 / 14;
+const rataRataSemester4 = jumlahSemester4 / 14;
+const rataRataSemester5 = jumlahSemester5 / 14;
+
+document.getElementById("rataRataSemester1").innerText = rataRataSemester1.toFixed(2);
+document.getElementById("rataRataSemester2").innerText = rataRataSemester2.toFixed(2);
+document.getElementById("rataRataSemester3").innerText = rataRataSemester3.toFixed(2);
+document.getElementById("rataRataSemester4").innerText = rataRataSemester4.toFixed(2);
+// document.getElementById("rataRataSemester5").innerText = rataRataSemester5.toFixed(2);
+
+
+
+// RATA-RATA SEMESTER 1 - 5
+const jumlahRataRataSemester = jumlahTotalSemester / 92;
+document.getElementById("jumlahRataRataSemester").innerText = jumlahRataRataSemester.toFixed(2);
+
+
+
+// KENAIKAN TIAP SEMESTER
+const kenaikanSemester1 = 0;
+const kenaikanSemester2 = rataRataSemester2 - rataRataSemester1;
+const kenaikanSemester3 = rataRataSemester3 - rataRataSemester2;
+const kenaikanSemester4 = rataRataSemester4 - rataRataSemester3;
+// const kenaikanSemester5 = rataRataSemester5 - rataRataSemester4;
+
+document.getElementById("kenaikanSemester1").innerText = "+" + kenaikanSemester1.toFixed(2);
+document.getElementById("kenaikanSemester2").innerText = "+" + kenaikanSemester2.toFixed(2);
+document.getElementById("kenaikanSemester3").innerText = "+" + kenaikanSemester3.toFixed(2);
+document.getElementById("kenaikanSemester4").innerText = "+" + kenaikanSemester4.toFixed(2);
+// document.getElementById("kenaikanSemester5").innerText = "+" + kenaikanSemester5.toFixed(2);
+
+
+
+// KENAIKAN RATA-RATA SEMESTER 1-5
+const jumlahKenaikanSemester = (kenaikanSemester1 + kenaikanSemester2 + kenaikanSemester3 + kenaikanSemester4) / 3;
+document.getElementById("jumlahKenaikanSemester").innerText = "+" + jumlahKenaikanSemester.toFixed(2);
+
+
+
+// TOTAL NILAI TIAP MATA PELAJARAN
+const hitungTotal = (arr) => arr.reduce((total, nilai) => total + nilai, 0);
+
+const paiTotal = hitungTotal(pai);
+const ppknTotal = hitungTotal(ppkn);
+const bindTotal = hitungTotal(bind);
+const mtkwTotal = hitungTotal(mtkw);
+const sjrTotal = hitungTotal(sjr);
+const bingTotal = hitungTotal(bing);
+const snbTotal = hitungTotal(snb);
+const pjokTotal = hitungTotal(pjok);
+const pkwuTotal = hitungTotal(pkwu);
+const mtkpTotal = hitungTotal(mtkp);
+const bioTotal = hitungTotal(bio);
+const fisTotal = hitungTotal(fis);
+const kimTotal = hitungTotal(kim);
+const ekoTotal = hitungTotal(eko);
+const geoTotal = hitungTotal(geo);
+const mloTotal = hitungTotal(mlo);
+
+
+
+// RATA-RATA TIAP MATA PELAJARAN
+const totalSemester = 6;
+
+const paiRataRata = paiTotal / totalSemester;
+const ppknRataRata = ppknTotal / totalSemester;
+const bindRataRata = bindTotal / totalSemester;
+const mtkwRataRata = mtkwTotal / totalSemester;
+const sjrRataRata = sjrTotal / totalSemester;
+const bingRataRata = bingTotal / totalSemester;
+const snbRataRata = snbTotal / totalSemester;
+const pjokRataRata = pjokTotal / totalSemester;
+const pkwuRataRata = pkwuTotal / totalSemester;
+const mtkpRataRata = mtkpTotal / totalSemester;
+const bioRataRata = bioTotal / totalSemester;
+const fisRataRata = fisTotal / totalSemester;
+const kimRataRata = kimTotal / totalSemester;
+const ekoRataRata = ekoTotal / totalSemester;
+const geoRataRata = geoTotal / 4;
+const mloRataRata = mloTotal / 4;
+
+
+
+// URUTAM DATA TIAP MATA PELAJARAN
+var dataNilai = [
+            { mapel: "Pendidikan Agama dan Budi Pekerti", nilai: paiTotal, rataRata: paiRataRata.toFixed(2) },
+            { mapel: "Pendidikan Pancasila dan Kewarganegaraan", nilai: ppknTotal, rataRata: ppknRataRata.toFixed(2) },
+            { mapel: "Bahasa Indonesia", nilai: bindTotal, rataRata: bindRataRata.toFixed(2) },
+            { mapel: "Matematika Wajib", nilai: mtkwTotal, rataRata: mtkwRataRata.toFixed(2) },
+            { mapel: "Sejarah Indonesia", nilai: sjrTotal, rataRata: sjrRataRata.toFixed(2) },
+            { mapel: "Bahasa Inggris", nilai: bingTotal, rataRata: bingRataRata.toFixed(2) },
+            { mapel: "Seni Budaya", nilai: snbTotal, rataRata: snbRataRata.toFixed(2) },
+            { mapel: "Pendidikan Jasmani, Olahraga, dan Kesehatan", nilai: pjokTotal, rataRata: pjokRataRata.toFixed(2) },
+            { mapel: "Prakarya dan Kewirausahaan", nilai: pkwuTotal, rataRata: pkwuRataRata.toFixed(2) },
+            { mapel: "Matematika Peminatan", nilai: mtkpTotal, rataRata: mtkpRataRata.toFixed(2) },
+            { mapel: "Biologi", nilai: bioTotal, rataRata: bioRataRata.toFixed(2) },
+            { mapel: "Fisika", nilai: fisTotal, rataRata: fisRataRata.toFixed(2) },
+            { mapel: "Kimia", nilai: kimTotal, rataRata: kimRataRata.toFixed(2) },
+            { mapel: "Ekonomi", nilai: ekoTotal, rataRata: ekoRataRata.toFixed(2) },
+            { mapel: "Geografi", nilai: geoTotal, rataRata: geoRataRata.toFixed(2) },
+            { mapel: "Muatan Lokal", nilai: mloTotal, rataRata: mloRataRata.toFixed(2) },
+        ];
+
+        dataNilai.sort(function(a, b) {
+            return b.nilai - a.nilai;
+        });
+
+var daftarNilaiElemen = document.getElementById("daftarNilai");
+
+for (var i = 0; i < dataNilai.length; i++) {
+    var tr = document.createElement("tr");
+    
+    var rank = document.createElement("td");
+    rank.textContent = (i + 1) + ".";
+    tr.appendChild(rank);
+
+    var mapel = document.createElement("td");
+    mapel.textContent = dataNilai[i].mapel;
+    tr.appendChild(mapel);
+
+    var nilai = document.createElement("td");
+    nilai.textContent = dataNilai[i].nilai;
+    tr.appendChild(nilai);
+
+    var rataRata = document.createElement("td");
+    rataRata.textContent = dataNilai[i].rataRata;
+    tr.appendChild(rataRata);
+
+    daftarNilaiElemen.appendChild(tr);
+}
+}
+subtituteData();
+
+
+
+// GRAFIK RATA-RATA NILAI TIAP SEMESTER
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 12;
+
+var speedCanvas = document.getElementById("speedChart");
+
+var speedData = {
+  labels: ["S1", "S2", "S3", "S4", "S5"],
+  datasets: [{
+    label: "Nilai",
+    data: [86.16, 87.47, 91.71, 95.43],
+    backgroundColor: 'rgba(30, 144, 255, 0.2)',
+    borderColor: '#1e90ff',
+    borderWidth: 1,
+    
+  }
+  , {
+    type: 'line',
+    label: 'Rata-Rata',
+    data: [88.86, 88.86, 88.86, 88.86],
+    fill: false,
+    backgroundColor: 'rgba(30, 144, 255, 0.2)',
+    borderColor: '#1e90ff',
+      borderWidth: 1,
+      hidden: true,
+  }]
+};
+
+var chartOptions = {
+  legend: {
+    display: true,
+    position: 'top',
+    labels: {
+      boxWidth: 50,
+      fontColor: 'black',
+    }
+  }
+};
+
+var lineChart = new Chart(speedCanvas, {
+  type: 'bar',
+  data: speedData,
+  options: chartOptions
+});
